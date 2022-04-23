09a4223a9bdb6f28ff0ccf466d64ffe2b5b1a014
GP <gp@envoy.com>
2022-04-23 13:41:41 -0700

    commit

--- dist/index.js
+++ dist/index.js
@@ -10742,18 +10742,43 @@ const main = async() => {
   try {
     const repository = core.getInput('repository', { required: true });
     const chartTag = core.getInput('chart-tag', { required: true });
+    const environment = core.getInput('environment', { required: true });
     updateChart(repository, chartTag);
+    updateValues(repository, chartTag, environment);
   } catch(error) {
     console.log(clc.red(error));
   }
 }
 
 function updateChart(repository, chartTag) {
-  const chart = loadYaml("charts/Chart.yaml")
-  chart.name = repository
-  chart.description = `Application ${repository} Helm chart to deploy on Kubernetes`
-  chart.version = chartTag
+  const chart = loadYaml("charts/Chart.yaml");
+  chart.name = repository;
+  chart.description = `Application ${repository} Helm chart to deploy on Kubernetes`;
+  chart.version = chartTag;
   writeYaml("charts/Chart.yaml", chart);
+}
+
+function updateValues(repository, chartTag, environment) {
+  const values = loadYaml("charts/values.yaml");
+  const input = loadYaml(`charts-config/values-${environment}.yaml`);
+  values.applicatioName = repository;
+  values.repository = repository;
+  values.tag = chartTag;
+  values.imagePullPolicy = "Always";
+  values.teamName = input.team;
+
+  if (input["deployment-strategy"] == "blue-green") {
+    values.deploymentStrategy.blueGreen.enabled = true;
+    values.deploymentStrategy.blueGreen.autoPromotionEnabled = false;
+    values.deploymentStrategy.blueGreen.maxUnavailable = 0;
+    values.deploymentStrategy.rolling.enabled = false;
+  } else if (input["deployment-strategy"] == "rolling") {
+    values.deploymentStrategy.blueGreen.enabled = false;
+    values.deploymentStrategy.rolling.enabled = true;
+  }
+
+  writeYaml("charts/values.yaml", values);
+  console.log(clc.green(yaml));
 }
 
 function loadYaml(file) {
