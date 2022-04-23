8795c29a037f284824e15c6a97c77ae5672ff033
GP <gp@envoy.com>
2022-04-23 11:19:19 -0700

    commit

--- dist/index.js
+++ dist/index.js
@@ -10742,14 +10742,26 @@ const main = async() => {
   try {
     const repository = core.getInput('repository', { required: true });
     const chartTag = core.getInput('chart-tag', { required: true });
-    const chart = yaml.load(fs.readFileSync("charts/Chart.yaml", 'utf8'));
-    chart.name = repository
-    chart.description = `Application ${repo} Helm chart to deploy on Kubernetes`
-    chart.version = chartTag
-    fs.writeFileSync("charts/Chart.yaml", yaml.dump(chart));
+    updateChart(repository, chartTag);
   } catch(error) {
     console.log(clc.red(error));
   }
+}
+
+function updateChart(repository, chartTag) {
+  const chart = loadYaml("charts/Chart.yaml")
+  chart.name = repository
+  chart.description = `Application ${repo} Helm chart to deploy on Kubernetes`
+  chart.version = chartTag
+  writeYaml("charts/Chart.yaml", chart);
+}
+
+function loadYaml(file) {
+  return yaml.load(fs.readFileSync(file, 'utf8'));
+}
+
+function writeYaml(file, data) {
+  fs.writeFileSync(file, yaml.dump(data));
 }
 
 main();
