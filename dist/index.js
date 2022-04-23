cb22aa0c53466115184850240acc39f1f6b5f56b
GP <gp@envoy.com>
2022-04-23 08:43:20 -0700

    commit

--- dist/index.js
+++ dist/index.js
@@ -10743,7 +10743,7 @@ const main = async() => {
     const repository = core.getInput('repository', { required: true });
     const chartTag = core.getInput('chart-tag', { required: true });
     const valuesYaml = yaml.load(fs.readFileSync("charts/Chart.yaml", 'utf8'));
-    const indentedJson = JSON.stringify(config, null, 4);
+    const indentedJson = JSON.stringify(valuesYaml, null, 4);
     console.log(clc.green(indentedJson));
   } catch(error) {
     console.log(clc.red(error));
